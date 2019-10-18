ec0b689d61cfeac0823bf41a613e9cea8c1b2da9
Javon Negahban <javon.negahban@cox.net>
2019-10-18 11:02:16 -0500

    Added display name for view model and cleaned up the Customer Index view

--- Project1.WebApp/Models/Customer.cs
+++ Project1.WebApp/Models/Customer.cs
@@ -1,16 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project1.WebApp.Models
 {
     public class Customer
     {
+        [DisplayName("ID")]
         [Required]
         public string Id { get; set; }
 
+        [DisplayName("First Name")]
         [Required]
         [Range(1, Project1.BusinessLogic.Customer.maxNameLength)]
         public string FirstName { get; set; }
 
+        [DisplayName("Last Name")]
         [Required]
         [Range(1, Project1.BusinessLogic.Customer.maxNameLength)]
         public string LastName { get; set; }
